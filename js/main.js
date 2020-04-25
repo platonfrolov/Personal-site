a0a9bb5bec0ac807501f65f3b501f6de7a784200
Platon Frolov <p.m.frolov@student.utwente.nl>
2020-04-25 17:02:17 +0200

    Navigation functionality done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+function showMenuText(el){
+    id = el.className.split(" ")[1]
+    document.getElementById(id).style.display = "block"
+}
 
+function hideMenuText(el){
+    id = el.className.split(" ")[1]
+    document.getElementById(id).style.display = "none"
+}
+
+function updateScrollIndicator(el){
+    console.log(window.innerHeight)
+    console.log(window.scrollY)
+    scrollRatio = window.scrollY / (window.innerHeight * 6);
+    document.getElementById("scroll-block").style.marginTop = scrollRatio * 240 + 'px';
+}
 
